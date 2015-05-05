0eb030a57d8c6c9e353fbc136ff05ca4ea2f45bf
Aleksandr Vasylchenko <vasylchenko.aleksander@gmail.com>
2015-05-05 20:57:42 +0300

    Initial Commit

---
+++ VAP IDPCources/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  VAP IDPCources
+//
+//  Created by Aleksandr Vasylchenko on 05.05.15.
+//  Copyright (c) 2015 Aleksandr Vasylchenko. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
