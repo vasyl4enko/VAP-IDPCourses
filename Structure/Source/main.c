eac62f67b5cdacaee0d4f91e88c1a2e08da8d26e
Aleksandr Vasylchenko <vasylchenko.aleksander@gmail.com>
2015-05-10 00:36:02 +0300

    main.c -> replacing. first commit.

---
+++ Structure/Source/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Structure
+//
+//  Created by Aleksandr Vasylchenko on 10.05.15.
+//  Copyright (c) 2015 Aleksandr Vasylchenko. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
