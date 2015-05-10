885a30c5e75d5e63f1725e143e73918c21198974
Aleksandr Vasylchenko <vasylchenko.aleksander@gmail.com>
2015-05-10 23:29:59 +0300

    BytesNumbersToConsol.c -> make function which printing bit's numbers to consol

---
+++ Structure/Source/BytesNymbersToConsol/BytesNumbersToConsol.c
@@ -0,0 +1,36 @@
+//
+//  BytesNumbersToConsol.c
+//  VAP IDPCources
+//
+//  Created by Aleksandr Vasylchenko on 10.05.15.
+//  Copyright (c) 2015 Aleksandr Vasylchenko. All rights reserved.
+//
+
+#include "BytesNumbersToConsol.h"
+#include <string.h>
+
+static const long long maskOfLeftBit = 0b1000000000000000000000000000000000000000000000000000000000000000;
+static const int countOfBits = 64;
+static const int lengthOfString = 65;
+
+
+void func(long long value){
+    
+    long long var = 0;
+    int iterator = 0;
+    char result[lengthOfString];
+    while (iterator < countOfBits) {
+        
+        if(iterator){
+            var = maskOfLeftBit & value<<iterator;
+            var == 0 ? strcat(result, "0") : strcat(result, "1");
+        }else{
+            var = maskOfLeftBit & value;
+            var == 0 ? strcat(result, "0") : strcat(result, "1");
+        }
+        
+        iterator++;
+    }
+    
+    printf("%s\n",result);
+}
---
+++ Structure/Source/BytesNymbersToConsol/BytesNumbersToConsol.h
@@ -0,0 +1,14 @@
+//
+//  BytesNumbersToConsol.h
+//  VAP IDPCources
+//
+//  Created by Aleksandr Vasylchenko on 10.05.15.
+//  Copyright (c) 2015 Aleksandr Vasylchenko. All rights reserved.
+//
+
+#ifndef __VAP_IDPCources__BytesNumbersToConsol__
+#define __VAP_IDPCources__BytesNumbersToConsol__
+
+#include <stdio.h>
+
+#endif /* defined(__VAP_IDPCources__BytesNumbersToConsol__) */
