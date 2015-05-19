dd78580b2867d6c5c1b9c1f394650bb858e1e28c
Aleksandr Vasylchenko <vasylchenko.aleksander@gmail.com>
2015-05-19 20:40:34 +0300

    VAPArray.c -> add empty file for struct witch will have array with reference count

---
+++ Humanoid/Source/VAPArray/VAPArray.c
@@ -0,0 +1,9 @@
+//
+//  VAPArray.c
+//  VAP IDPCources
+//
+//  Created by Aleksandr Vasylchenko on 19.05.15.
+//  Copyright (c) 2015 Aleksandr Vasylchenko. All rights reserved.
+//
+
+#include "VAPArray.h"
---
+++ Humanoid/Source/VAPArray/VAPArray.h
@@ -0,0 +1,14 @@
+//
+//  VAPArray.h
+//  VAP IDPCources
+//
+//  Created by Aleksandr Vasylchenko on 19.05.15.
+//  Copyright (c) 2015 Aleksandr Vasylchenko. All rights reserved.
+//
+
+#ifndef __VAP_IDPCources__VAPArray__
+#define __VAP_IDPCources__VAPArray__
+
+#include <stdio.h>
+
+#endif /* defined(__VAP_IDPCources__VAPArray__) */
