93481749c69818c9fd38397db8e5dd1f4d9246b4
Aleksandr Vasylchenko <vasylchenko.aleksander@gmail.com>
2015-05-20 19:51:01 +0300

    VAPStringTests.c -> add emty test file

---
+++ Humanoid/Tests/VAPStringTests/VAPStringTests.c
@@ -0,0 +1,9 @@
+//
+//  VAPStringTests.c
+//  VAP IDPCources
+//
+//  Created by Aleksandr Vasylchenko on 20.05.15.
+//  Copyright (c) 2015 Aleksandr Vasylchenko. All rights reserved.
+//
+
+#include "VAPStringTests.h"
---
+++ Humanoid/Tests/VAPStringTests/VAPStringTests.h
@@ -0,0 +1,14 @@
+//
+//  VAPStringTests.h
+//  VAP IDPCources
+//
+//  Created by Aleksandr Vasylchenko on 20.05.15.
+//  Copyright (c) 2015 Aleksandr Vasylchenko. All rights reserved.
+//
+
+#ifndef __VAP_IDPCources__VAPStringTests__
+#define __VAP_IDPCources__VAPStringTests__
+
+#include <stdio.h>
+
+#endif /* defined(__VAP_IDPCources__VAPStringTests__) */
