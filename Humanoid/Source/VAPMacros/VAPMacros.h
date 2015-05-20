e447c2da9315c7da138fb7dec7a25358467465f5
Aleksandr Vasylchenko <vasylchenko.aleksander@gmail.com>
2015-05-20 22:14:59 +0300

    VAPMacros.h -> add macros assign for setters structures

---
+++ Humanoid/Source/VAPMacros/VAPMacros.h
@@ -0,0 +1,19 @@
+//
+//  VAPMacros.h
+//  VAP IDPCources
+//
+//  Created by Aleksandr Vasylchenko on 20.05.15.
+//  Copyright (c) 2015 Aleksandr Vasylchenko. All rights reserved.
+//
+
+#ifndef VAP_IDPCources_VAPMacros_h
+#define VAP_IDPCources_VAPMacros_h
+
+#define
+
+#define VAPMacrosAssign(object, element) \
+    if(NULL != object) { \
+        object->_##element = element; \
+    }
+
+#endif
