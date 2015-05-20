d982216bff31f3c66d80445da3f6656846adfa45
Aleksandr Vasylchenko <vasylchenko.aleksander@gmail.com>
2015-05-20 19:44:35 +0300

    VAPArrayTests.c -> add empty test file.

---
+++ Humanoid/Tests/VAPArrayTests/VAPArrayTests.c
@@ -0,0 +1,9 @@
+//
+//  VAPArrayTests.c
+//  VAP IDPCources
+//
+//  Created by Aleksandr Vasylchenko on 20.05.15.
+//  Copyright (c) 2015 Aleksandr Vasylchenko. All rights reserved.
+//
+
+#include "VAPArrayTests.h"
